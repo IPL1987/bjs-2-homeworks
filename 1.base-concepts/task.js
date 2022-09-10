b8ab45e0cc8006bd6283250a754032689339119e
IPL87 <gmax_lg@ukr.net>
2022-09-10 18:48:12 +0300

    fixed mistakes

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,7 +5,7 @@ function solveEquation(a, b, c) {
   let discriminant = Math.pow(b, 2) - 4 * a * c;
 
   if (discriminant < 0) {
-  } else if (discriminant == 0) {
+  } else if (discriminant === 0) {
     arr = [-b / (2 * a)]
   } else if (discriminant > 0) {
     arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
@@ -17,14 +17,14 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
   if (isNaN(percent)) {
-   return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
   }
   else if (isNaN(contribution)) {
     return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
   }
   else if (isNaN(amount)) {
     return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
-  } 
+  }
 
   function monthDiff(d1, d2) {
     let months;
@@ -34,15 +34,15 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     return months <= 0 ? 0 : months;
   }
 
-  let totalAmount
+  let totalAmount;
   let creditBody = amount - contribution;
-  let mortgageRate = (percent / 100) * (1 / 12)
-  let data2 = new Date()
-  let data1 = new Date(date)
+  let mortgageRate = (percent / 100 / 12);
+  let data2 = new Date();
+  let data1 = new Date(date);
   let loanTerm = monthDiff(data2, data1);
-  totalAmount = (creditBody * (mortgageRate + (mortgageRate / (((1 + mortgageRate) ** loanTerm) - 1)))) * loanTerm
-  totalAmount = (Math.round(totalAmount * 100) / 100)
-  console.log(totalAmount)
+  totalAmount = (creditBody * (mortgageRate + (mortgageRate / (((1 + mortgageRate) ** loanTerm) - 1)))) * loanTerm;
+  totalAmount = (Math.round(totalAmount * 100) / 100);
+  console.log(totalAmount);
 
   return totalAmount;
 }
---
+++ 111.js
@@ -0,0 +1,3 @@
+let date = new Date('December 25, 1995 23:15:30').getDate();
+
+console.log(date);
