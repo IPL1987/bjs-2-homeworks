1375bb71202dd43efd295ced266cae1d5568cd77
IPL87 <gmax_lg@ukr.net>
2022-09-15 10:28:42 +0300

    fixed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,9 @@
 'use strict'
 
 function compareArrays(arr1, arr2) {
-  let result = arr1.every((elem, index) => elem === arr2[index]) && arr1.length === arr2.length;
-
-  return result;
+  return arr1.every((elem, index) => elem === arr2[index]) && arr1.length === arr2.length;
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter(elem => elem > 0 && elem % 3 === 0).map(elem => elem * 10);
-
-  return resultArr;
+  return arr.filter(elem => elem > 0 && elem % 3 === 0).map(elem => elem * 10);
 }
