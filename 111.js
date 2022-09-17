f67d113ba5367d9ccea441ba41e86eda13375285
IPL87 <gmax_lg@ukr.net>
2022-09-17 15:52:41 +0300

    Initial commit

--- 111.js
+++ 111.js
@@ -5,5 +5,31 @@
 // }
 // array(456, 3453)
 // array('456', '4563')
-let array=[1,2,3,4,5,667]
-array.forEach((element, index, array) => console.log(element, index, array));
+
+// let array=[1,2,3,4,5,667]
+// array.forEach((element, index, array) => console.log(element, index, array));
+
+function print(first, seccond, ...args) {
+  console.log(first, seccond)
+  for (let i in args) {
+    console.log( i)
+  }
+}
+print(1, 1, 1, 55, 3, 7, 6, 9, 3, 7)
+
+// function result (...params) {
+//   let sum = 0;
+//   for (let i = 0; i < params.length; i++) {
+//     sum += params[i];
+//   }
+//   console.log (sum)
+// }
+// result(1,4,6,23,8,6)
+
+// function hell() {
+//   return hello();
+// }
+// function hello() {
+//   console.log("привет")
+// }
+// hell ();
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,33 @@
+'use strict'
 function Student(name, gender, age) {
-    // Ваш код
-
 }
+
+let student1 = new Student('first', 'male', '18');
+let student2 = new Student('secons', 'male', '20');
+let student3 = new Student('third', 'female', '18');
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  this.marks === undefined ? this.marks = [mark] : this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks === undefined ? this.marks = [...marks] : this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+  let avg = 0;
+  for (let elem in this.marks){
+    avg += this.marks[elem]
+  }
+  return avg / this.marks.length
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
