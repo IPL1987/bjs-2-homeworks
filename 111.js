5918243e52215314e783b93580b4b0e6c81920d1
IPL87 <gmax_lg@ukr.net>
2022-10-07 12:31:24 +0300

    1

--- 111.js
+++ 111.js
@@ -51,21 +51,113 @@
 // ]
 // console.log(persons)
 
-function async() {
-  const element = new Date();
-  setTimeout(() => {
-    const now = new Date()
-    console.log(now - element)
-  }, 1000)
+// function async() {
+//   const element = new Date();
+//   setTimeout(() => {
+//     const now = new Date()
+//     console.log(now - element)
+//   }, 1000)
+// }
+// function sum() {
+//   let sum = 0;
+//   for (let i = 0; i < 1000000; i++) {
+//     sum += 1;
+//   }
+//   return sum
+// }
+// for (let i = 0; i < 10; i++) {
+// async();
+// //sum();
+// }
+// function cachingDecoratorNew(func) {
+//   let cache = [];
+//   function wrapper(...args) {
+//     let hash = args.join(','); // получаем правильный хэш
+//     let objectInCache = cache.find(cacheItem => cacheItem === hash); // ищем элемент, хэш которого равен нашему хэшу
+//     if (objectInCache) {
+//       console.log('Из кэша: ' + objectInCache.value);
+//       return 'Из кэша: ' + objectInCache.value;
+//     }
+//     let value = func.call(this, ...args);
+//     console.log('Вычисляем: ' + value);
+//     cache.push({ hash, value }); // добавляем элемент с правильной структурой
+//     if (cache.length > 5) {
+//       cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
+//     }
+//     return 'Вычисляем: ' + value;
+//   }
+//   return wrapper;
+// }
+function debounceDecoratorNew(func) {
+  // Ваш код
 }
-function sum() {
-  let sum = 0;
-  for (let i = 0; i < 1000000; i++) {
-    sum += 1;
-  }
-  return sum
-}
-for (let i = 0; i < 10; i++) {
-async();
-//sum();
+
+// //Задача 2. Debounce декоратор с моментальным вызовом
+// function debounceDecoratorNew(func, ms) {
+//   // Ваш код
+//   let timeout;
+//   let firstCall = true;
+
+//   return function (...rest) {
+//     if (firstCall) {
+//       func.call(this, ...rest);
+//       firstCall = false;
+//       return
+//     }
+
+//     clearTimeout(timeout);
+
+//     timeout = setTimeout(() => {
+//       func.call(this, ...rest);
+//       firstCall = true;
+//     }, ms);
+//   };
+// }
+
+// //Задача 3. Усовершенствуйте debounceDecoratorNew
+// function debounceDecorator2(func, ms) {
+//     // Ваш код
+//     let timeout;
+//     let firstCall = true;
+
+//     function wrapper(...rest) {
+//       if (firstCall) {
+//         func.call(this, ...rest);
+//         firstCall = false;
+//         wrapper.count++;
+//         return
+//       }
+
+//       clearTimeout(timeout);
+
+//       timeout = setTimeout(() => {
+//         func.call(this, ...rest);
+//         firstCall = true;
+//         wrapper.count++;
+//       }, ms);
+//     };
+
+//     wrapper.count = 0;
+
+//     return wrapper;
+// }
+function debounceDecoratorNew(func, ms) {
+  let timeout = null;
+  function wrapper(...args) {
+    if (timeout) {
+      wrapper.count++;
+      wrapper.allCount.push(args);
+      return func.call(this, ...args);
+    }
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      timeout = null;
+      func.call(this, ...rest);
+      wrapper.count++;
+    }, ms);
+  };
+  wrapper.allCount = [];
+
+  return wrapper;
 }
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,40 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    let hash = args.join(','); // получаем правильный хэш
+    let objectInCache = cache.find(cacheItem => cacheItem.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
+    if (objectInCache) {
+      console.log('Из кэша: ' + objectInCache.value);
+      return 'Из кэша: ' + objectInCache.value;
+    }
+    let value = func.call(this, ...args);
+    console.log('Вычисляем: ' + value);
+    cache.push({ hash, value }); // добавляем элемент с правильной структурой
+    if (cache.length > 5) {
+      cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
+    }
+    return 'Вычисляем: ' + value;
+  }
+  return wrapper;
 }
 
+function debounceDecoratorNew(func, ms) {
+  let timeout = null;
+  function wrapper(...args) {
+    if (timeout) {
+      wrapper.count++;
+      wrapper.allCount.push(args);
+      return ;
+    }
+    clearTimeout(timeout);
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+    timeout = setTimeout(() => {
+      timeout = null;
+      wrapper.count++;
+    }, ms);
+  };
+  wrapper.count = 0;
+  wrapper.allCount = [];
+
+  return wrapper;
 }
